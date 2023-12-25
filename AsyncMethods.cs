e3e5ffa8439432d428d29ef87344f00a31602609
Selconag <ogrengeek@gmail.com>
2023-12-25 17:16:06 +0300

    More Test Have Been Run

--- AsyncMethods.cs
+++ AsyncMethods.cs
@@ -49,7 +49,25 @@ public class AsyncMethods
 
     }
 
-    public static async Task FirstRun()
+    public static string Turret2_Alt()
+    {
+        return "Firing Howitzer is over";
+    }
+
+    public static string Turret3_Alt()
+    {
+        return "Firing Railgun is over";
+
+    }
+
+
+    // Here's our (hypothetical) expensive calculation
+    static int PerformExpensiveCalculation(int value)
+    {
+        return value * value; // For simplicity, let's just square it
+    }
+
+    public static async Task FirstExample()
     {
         Console.WriteLine("----------First Run-----------");
         Console.WriteLine("Press a button to start.");
@@ -90,7 +108,7 @@ public class AsyncMethods
     /// Is to reassign them. It may look primite but it is what it is
     /// </summary>
     /// <returns></returns>
-    public static async Task SecondRun()
+    public static async Task SecondExample()
     {
         Console.WriteLine("----------Second Run-----------");
         Console.WriteLine("Press a button to start.");
@@ -107,7 +125,7 @@ public class AsyncMethods
         Console.WriteLine($"All turrets finished on {methodName}");
     }
 
-    public static async Task ThirdRun()
+    public static async Task ThirdExample()
     {
         Console.WriteLine("----------Third Run-----------");
         Console.WriteLine("This process is started automatically");
@@ -157,17 +175,184 @@ public class AsyncMethods
 
     }
 
-    public static async Task RunTaskAsync(Func<Task> taskMethod)
+    public static Task FourthExample()
     {
-        Task task = taskMethod();
-        await task;
+        Console.WriteLine("----------Fourth Run | Non Async-----------");
+        Console.WriteLine("Press a button to start.");
+        Console.ReadKey();
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+
+        // Initialize an array
+        int[] data = Enumerable.Range(0, 1000000000).ToArray();
+
+        foreach (var number in data)
+        {
+            //Console.WriteLine(PerformExpensiveCalculation(number));
+            PerformExpensiveCalculation(number);
+        }
+
+        watch.Stop();
+        Console.WriteLine($"Performed in: {watch.ElapsedMilliseconds} milliseconds");
+
+        return Task.FromResult(data);
     }
 
-    public static void AssignTaskAsync(Task task, Func<Task> method)
+    public static async Task FourthExampleAsync()
     {
-        task = null;
-        task = method();
+        Console.WriteLine("----------Fourth Run | Async-----------");
+        Console.WriteLine("Press a button to start.");
+        Console.ReadKey();
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        // Initialize an array
+        int[] data = Enumerable.Range(0, 1000000000).ToArray();
+
+
+        // Transform the data in parallel
+        Parallel.For(0, data.Length, i =>
+        {
+            // This calculation takes place in parallel
+            data[i] = PerformExpensiveCalculation(data[i]);
+        });
+        watch.Stop();
+        Console.WriteLine($"Performed in: {watch.ElapsedMilliseconds} milliseconds");
     }
 
-   
+
+    public static async Task FifthExample()
+    {
+        // Declare the tasks
+        Task[] tasks = new Task[10];
+
+        // Initialize and start the tasks
+        for (var i = 0; i < 10; i++)
+        {
+            int taskNum = i; // To avoid closure on the loop variable
+            tasks[i] = new Task(() =>
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"Task {taskNum} complete!");
+            });
+        }
+
+        // Now, let’s start our tasks
+        foreach (var task in tasks)
+        {
+            task.Start();
+        }
+
+        // And wait for all of them to complete
+        Task.WaitAll(tasks);
+    }
+
+    public static async Task SixthExample()
+    {
+        // This example represent multiple asynchronous tasks
+        List<Task> tasks = new List<Task>();
+        for (int i = 0; i < 2000; i++)
+        {
+            int taskNum = i;  // To avoid the closure variable trap
+            tasks.Add(Task.Run(async () =>
+            {
+                // Simulate a long-running task
+                await Task.Delay(1000);
+                Console.WriteLine($"Task {taskNum} completed!");
+            }));
+        };
+        Console.WriteLine("Waiting for all tasks to complete...");
+        await Task.WhenAll(tasks);
+        Console.WriteLine("All tasks completed!");
+    }
+
+    public static async Task SeventhExample()
+    {
+        // Create a TaskCompletionSource
+        var tcs = new TaskCompletionSource<bool>();
+
+        // Start a task that waits for user input
+        Task someTask = Task.Run(() =>
+        {
+            // Callback to signal that the operation has completed
+            tcs.SetResult(true); // You can also use tcs.SetException to signal an error
+        });
+
+
+        // Now you can await the task
+        await tcs.Task; //Equivalent of calling directly someTask
+
+        // Output: Operation Complete!
+        Console.WriteLine("Operation Complete!");
+    }
+
+    public static async Task SeventhExample_Alternative()
+    {
+        // Create a TaskCompletionSource
+        var tcs = new TaskCompletionSource<int>();
+
+        // Start a task that waits for user input
+        Task.Run(() =>
+        {
+            Console.WriteLine("Enter a number:");
+            string userInput = Console.ReadLine();
+
+            int number;
+            if (int.TryParse(userInput, out number))
+            {
+                // Set the result if parsing is successful
+                tcs.SetResult(number);
+            }
+            else
+            {
+                // Set an exception if parsing fails
+                tcs.SetException(new ArgumentException("Invalid input. Please enter a valid number."));
+            }
+        });
+
+        // Wait for the task completion
+        try
+        {
+            int result = await tcs.Task;
+            Console.WriteLine($"You entered: {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        // Now you can await the task
+        await tcs.Task;
+
+        // Output: Operation Complete!
+        Console.WriteLine("Operation Complete!");
+    }
+
+    //Bad example given by website, will be fixed later
+    public static async Task EightthExample()
+    {
+        // Dummy method that fetches and processes customer data
+        async Task ProcessCustomerDataAsync()
+        {
+            Console.WriteLine("Data processing started");
+
+            // Simulate time-consuming data processing
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+            Console.WriteLine("Data processing completed after 10 second.");
+        }
+
+        Timer timer = new Timer(state =>
+        {
+            Task.Run(async () => await ProcessCustomerDataAsync());
+        }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+        await ProcessCustomerDataAsync();
+    }
+
+    public static async Task NinthExample()
+    {
+        //Chaining tasks using async
+        var firstTask = new Task<string>(Turret3_Alt);
+        var secondTask = new Task<string>(Turret2_Alt);
+        firstTask.ContinueWith(t => secondTask.Start());
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -3,8 +3,39 @@ using AsyncTest;
 
 Console.WriteLine("Hello, There!");
 
-AsyncMethods.FirstRun();
+//NOTE: First and second examples are working asynchronously because the second method uses await and firt one is not
+//usage of await keyword can actually kill the async proccess if we need to continue on.
+//but must be used if we need to receive some kind of input from the method that is awaited.
+//AsyncMethods.FirstExample();
 
-await AsyncMethods.SecondRun();
+//await AsyncMethods.SecondExample();
 
-await AsyncMethods.ThirdRun();
+//await AsyncMethods.ThirdExample();
+
+////IMPORTANT NOTE: While running test its found as (value > 10^9) values of array is performant to use parralel calculations.
+////Not parallel version of 4th example
+//await AsyncMethods.FourthExample();
+
+////Parallel version of 4th example
+////var newVar = 
+//    await AsyncMethods.FourthExampleAsync();
+
+//await AsyncMethods.FifthExample();
+
+//await AsyncMethods.SixthExample();
+
+////Example usage of TaskCompletionSource's
+////NOTE: Alternative is a better example for any use case scenario
+////await AsyncMethods.SeventhExample();
+//await AsyncMethods.SeventhExample_Alternative();
+
+////Exampel for repeating processes
+//await AsyncMethods.EightthExample();
+
+await AsyncMethods.NinthExample();
+
+
+
+
+
+
